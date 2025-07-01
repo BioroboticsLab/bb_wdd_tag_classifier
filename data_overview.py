41a7fba1bc4907440916a85f0ff7ef8e612b4bdc
Koyomi12 <38738998+Koyomi12@users.noreply.github.com>
2025-07-01 21:20:15 +0200

    Use CLI args to replace hardcoded paths in data_overview.py

--- data_overview.py
+++ data_overview.py
@@ -1,9 +1,12 @@
+import argparse
 import json
+from pathlib import Path
+from typing import Optional
 from zipfile import ZipFile
 
 from tqdm import tqdm
 
-from daily_data_processing import ZIPS_PATH, is_wood_in_frame
+from daily_data_processing import is_wood_in_frame
 
 
 def main():
@@ -11,8 +14,11 @@ def main():
     Gives a daily overview of the number of detections, videos  per dance type
     and how many videos are filtered out by the wood filter.
     """
+    parser = init_argparse()
+    args: MyArgs = parser.parse_args(namespace=MyArgs())
+    zipped_wdd_data_dir = Path(args.zipped_wdd_data_dir)
     data = dict()
-    zip_files = sorted(list(ZIPS_PATH.rglob("*")))
+    zip_files = sorted(list(zipped_wdd_data_dir.rglob("*")))
     for zip_path in tqdm(zip_files):
         if not zip_path.suffix == ".zip":
             continue
@@ -42,6 +48,25 @@ def main():
         data[date_str] = day_data
     with open("output/data_overview.json", "w") as file:
         json.dump(data, file, indent=2)
+
+
+class MyArgs(argparse.Namespace):
+    zipped_wdd_data_dir: Path
+    woodfilter: Optional[bool]
+
+
+def init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description=(
+            "Gives a daily overview of the number of detections and videos per dance type and how many videos would be filtered out by the wood filter"
+        ),
+    )
+    parser.add_argument(
+        "zipped_wdd_data_dir",
+        type=Path,
+        help="path to directory containing zip archives of WDD detection data (APNG video snippets and JSON metadata)",
+    )
+    return parser
 
 
 if __name__ == "__main__":
