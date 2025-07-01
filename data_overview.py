26889019c0834ffcae3b23be4e9614f93fdb1704
Koyomi12 <38738998+Koyomi12@users.noreply.github.com>
2025-07-01 22:02:55 +0200

    Add optional CLI argument to apply woodfilter for data_overview.py

--- data_overview.py
+++ data_overview.py
@@ -17,6 +17,7 @@ def main():
     parser = init_argparse()
     args: MyArgs = parser.parse_args(namespace=MyArgs())
     zipped_wdd_data_dir = Path(args.zipped_wdd_data_dir)
+    apply_woodfilter = args.woodfilter
     data = dict()
     zip_files = sorted(list(zipped_wdd_data_dir.rglob("*")))
     for zip_path in tqdm(zip_files):
@@ -24,7 +25,11 @@ def main():
             continue
         predicted_class_labels = ["waggle", "activating", "ventilating", "other"]
         day_data = {
-            label: {"detections": 0, "videos": 0, "wood filter": 0}
+            label: {
+                "detections": 0,
+                "videos": 0,
+                **({"wood filter": 0} if apply_woodfilter else {}),
+            }
             for label in predicted_class_labels
         }
         with ZipFile(zip_path) as zip_file:
@@ -37,7 +42,7 @@ def main():
                     json_data = json.load(metadata_file)
                 label = json_data["predicted_class_label"]
                 day_data[label]["detections"] += 1
-                if is_wood_in_frame(json_data):
+                if apply_woodfilter and is_wood_in_frame(json_data):
                     day_data[label]["wood filter"] += 1
 
                 # find matching video file
@@ -65,6 +70,11 @@ def init_argparse() -> argparse.ArgumentParser:
         "zipped_wdd_data_dir",
         type=Path,
         help="path to directory containing zip archives of WDD detection data (APNG video snippets and JSON metadata)",
+    )
+    parser.add_argument(
+        "--woodfilter",
+        action="store_true",
+        help="Enable filtering and counting of detections with wood in the frame",
     )
     return parser
 
